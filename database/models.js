1da0a6e7c749fb39f66a1132696bad14c7a8e55d
kayqueR69 <kayquericharlyson@gmail.com>
2024-05-10 17:13:01 -0300

    wip : modificação

--- database/models.js
+++ database/models.js
@@ -1,5 +1,23 @@
-import { Sequelize } from "sequelize";
-import { db } from "./dbconfig.js";
+import Sequelize from "sequelize";
+import Dot from 'dotenv'
+Dot.config()
+
+const dbConfig = {
+    nome : process.env.DB_NAME, 
+    host : process.env.DB_HOST, 
+    user : process.env.DB_USER, 
+    pass : process.env.DB_PASS
+}
+
+export const db = new Sequelize(dbConfig.nome, dbConfig.user, dbConfig.pass, {
+    host: dbConfig.host,
+    dialect : "mysql", 
+    define : {
+        timestamps : false,
+        freezeTableName : true
+    }
+})
+
 
 const professor = db.define('professor', {
 
