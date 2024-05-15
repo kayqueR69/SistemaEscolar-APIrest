8e818b08c80a9ecaefdb8200ed6623298a511fd6
kayqueR69 <kayquericharlyson@gmail.com>
2024-05-15 08:05:52 -0300

    alt

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -14,25 +14,25 @@ router.get('/', (req,res) => {
 
 router.post('/prof/cadastro', rotasProf.cadastro)
 router.post('/prof/login', rotasProf.login)
-router.post('/prof/alterar', rotasProf.alterarDado)
+router.put('/prof/alterar', rotasProf.alterarDado)
 router.delete('/prof/deleteuser/:id', rotasProf.deleteUserProf)
 router.post('/prof/criarturma', rotasProf.criarTurma)
 router.get('/prof/alunos/:idTurma', rotasProf.getAlunos)
 router.delete('/prof/delturma', rotasProf.deletarTurma)
 router.post('/prof/adddisciplina', rotasProf.addDisciplina)
 router.get('/prof/getdisciplinas/:idTurma', rotasProf.getDisciplinas)
-router.post('/prof/updatedisciplina', rotasProf.updateDisciplina)
+router.put('/prof/updatedisciplina', rotasProf.updateDisciplina)
 router.delete('/prof/deldisciplina', rotasProf.deletarDisciplina)
-router.post('/prof/atribuirnota', rotasProf.atribuirNota)
+router.put('/prof/atribuirnota', rotasProf.atribuirNota)
 
 // rotas de aluno
 
 router.post('/aluno/cadastro', rotasAluno.cadastro)
 router.post('/aluno/login', rotasAluno.login)
-router.post('/aluno/altnome', rotasAluno.alterarNome)
+router.put('/aluno/altnome', rotasAluno.alterarNome)
 router.delete('/aluno/deluser/:id', rotasAluno.deleteUser)
 router.get('/aluno/verturmas', rotasAluno.verTurmas)
-router.post('/aluno/matricula', rotasAluno.vincularTurma)
+router.put('/aluno/matricula', rotasAluno.vincularTurma)
 router.get('/aluno/infos/:id/:idTurma', rotasAluno.infosAluno)
 
 // --------------------------------------------
